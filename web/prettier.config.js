5faa2ea511d7790bca432d3c6fbee53bae169e8b
sqmasep <leblond.romain@orange.fr>
2024-01-03 02:00:57 +0100

    [CONFIG] Some config files ok

---
+++ web/prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  arrowParens: "avoid",
+  printWidth: 80,
+};
