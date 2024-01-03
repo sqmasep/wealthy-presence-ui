192f87cb2821518e5a46faba77d9edbb246fa7ec
sqmasep <leblond.romain@orange.fr>
2024-01-03 01:59:24 +0100

    [CLEANUP] Removed unused JSX components & utils

--- src/utils/cn.ts
+++
@@ -1,5 +0,0 @@
-import type { CnOptions } from "tailwind-variants";
-import { cn as tvCn } from "tailwind-variants";
-
-export const cn = (...classes: CnOptions) =>
-  tvCn(...classes)({ twMerge: true });
