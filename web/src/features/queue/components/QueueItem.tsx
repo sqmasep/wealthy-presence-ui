a8e624f9f0d2b0e66a2499436c8d5e6d29c7d1e3
sqmasep <leblond.romain@orange.fr>
2024-01-14 00:08:45 +0100

    [FEAT] QueueItem mutation action

--- web/src/features/queue/components/QueueItem.tsx
+++ web/src/features/queue/components/QueueItem.tsx
@@ -1,5 +1,6 @@
 import { X } from "lucide-react";
 import { Button } from "~/components/ui/button";
+import { useMutateQueue } from "../hooks/useMutateQueue";
 
 export default function QueueItem({
   id,
@@ -8,9 +9,18 @@ export default function QueueItem({
   id: string;
   title?: string;
 }) {
+  const { mutate } = useMutateQueue();
+
   return (
     <div className="group relative rounded-lg hover:bg-zinc-200/5">
-      <button className="block w-full p-1 py-4 text-left">{title}</button>
+      <button
+        className="block w-full p-1 py-4 text-left"
+        onClick={() => {
+          mutate(id);
+        }}
+      >
+        {title}
+      </button>
       <Button
         variant="outline"
         className="absolute right-0 top-1/2 -translate-y-1/2 px-4"
