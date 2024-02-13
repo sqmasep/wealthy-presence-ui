f324e9eaec0e23666ae916bed9fb5273bdb3bc5f
sqmasep <leblond.romain@orange.fr>
2024-02-13 14:48:41 +0100

    [FEAT] Added some config and changed activity route

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,8 +9,14 @@ import presetRouterBuilder from "./routes/preset";
 import queueRouterBuilder from "./routes/queue";
 import statusRouterBuilder from "./routes/status";
 import listsRouterBuilder from "./routes/lists";
+import { activityRouterBuilder } from "./routes/activity";
 
-function withWeb(presence: WealthyPresence, config?: { port?: number }) {
+interface WealthyPresenceUIConfig {
+  port?: number;
+  storage?: string;
+}
+
+function withWeb(presence: WealthyPresence, config?: WealthyPresenceUIConfig) {
   const wss = new WebSocketServer({
     port: 4233,
   });
@@ -31,8 +37,6 @@ function withWeb(presence: WealthyPresence, config?: { port?: number }) {
     });
   });
 
-  // const defaultQueue = presence.getQueue();
-
   const app = new Hono()
     // CORS
     .use("*", cors())
@@ -49,50 +53,8 @@ function withWeb(presence: WealthyPresence, config?: { port?: number }) {
     .route("/queue", queueRouterBuilder(presence))
     .route("/preset", presetRouterBuilder(presence))
     .route("/lists", listsRouterBuilder(presence))
-    .route("/status", statusRouterBuilder(presence));
-
-  // .post("/presets", c => {
-  //   presence.addPreset(createPreset({ title: "test" }));
-  //   return c.json(presence.getPresets(), 201);
-  // })
-  // .post("/set-preset", idValidator, async c => {
-  //   const foundPreset = defaultQueue.find(
-  //     preset => preset.id === c.req.valid("json").id,
-  //   );
-
-  //   if (!foundPreset) return c.json({ message: "Preset not found" }, 404);
-
-  //   presence.setPresets([foundPreset]);
-  //   return c.json(presence.getPresets(), 200);
-  // })
-  // .delete("/delete-preset", async c => {
-  //   // TODO validate id (maybe use "/preset/:id" instead of "/delete-preset")
-  //   presence.removePreset((await c.req.json())?.id);
-  //   return c.json(presence.getPresets(), 200);
-  // })
-
-  // .post("/restore-state", c => {
-  //   presence.setPresets(defaultQueue);
-  //   return c.json(presence.getPresets(), 200);
-  // });
-
-  // .post("/run", async c => {
-  //   try {
-  //     await presence.run();
-  //     return c.json({ message: "Running" }, 200);
-  //   } catch (error) {
-  //     return c.json({ message: "Something went wrong" }, 500);
-  //   }
-  // })
-
-  // .post("/stop", async c => {
-  //   await presence.stop();
-  //   return c.json({ message: "Stopped" }, 200);
-  // })
-
-  // .get("/is-running", c => {
-  //   return c.json({ isRunning: presence.isRunning() }, 200);
-  // });
+    .route("/status", statusRouterBuilder(presence))
+    .route("/activity", activityRouterBuilder(presence));
 
   serve(
     {
@@ -142,9 +104,9 @@ const presence = new WealthyPresence({
     ]),
 
     createList("second list", [
-      createPreset({ title: "test", description: "test" }),
-      createPreset({ title: "test", description: "test" }),
-      createPreset({ title: "test", description: "test" }),
+      createPreset({ title: "test1", description: "test1" }),
+      createPreset({ title: "test2", description: "test2" }),
+      createPreset({ title: "test3", description: "test3" }),
     ]),
 
     createList("Utils", [
