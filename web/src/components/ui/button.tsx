51b6b7026014b51f5605218aa5e320b62b78cdce
sqmasep <leblond.romain@orange.fr>
2024-02-14 10:25:45 +0100

    [FEAT] Added defaultVariant.shape for button component

--- web/src/components/ui/button.tsx
+++ web/src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "~/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex active:scale-95 items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
@@ -28,14 +28,14 @@ const buttonVariants = cva(
       shape: {
         squared: "rounded-md",
         rounded: "rounded-full",
-      }
+      },
     },
     defaultVariants: {
       variant: "default",
       size: "default",
-      shape: "squared"
+      shape: "squared",
     },
-  },
+  }
 );
 
 export interface ButtonProps
@@ -55,7 +55,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         {...props}
       />
     );
-  },
+  }
 );
 Button.displayName = "Button";
 
