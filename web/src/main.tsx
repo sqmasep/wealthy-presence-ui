7b6cbbbf70a45824ccf6ca5d02de47a29e0df55b
sqmasep <leblond.romain@orange.fr>
2024-02-19 11:15:51 +0100

    [FEAT] Added toaster provider from shadcn/ui

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,11 +4,13 @@ import App from "./App.tsx";
 import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/react-query.ts";
+import { Toaster } from "./components/ui/sonner.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <Toaster />
     </QueryClientProvider>
   </React.StrictMode>
 );
